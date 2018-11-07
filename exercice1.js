9a2486c0107e66119cb1fa62873b7039e1cdb9b1
Max <maximeducampupto@gmail.com>
2018-11-07 06:12:39 +0100

    2nd commit:
    - CSS Tweaks,
    - Créé un objet listeItem chargé des opérations de création et de
    masquage des li en eux-mêmes (plutôt que demander à la liste de les
    créer, etc.),
    - Ajouté possibilité de réorganiser la liste (article vers le haut,
    vers le bas)
    
    TODO:
    - throwAlert(message, color) fonction pour prévenir d'un input
    trop large,
    - Trouver des bugs
    - Réécrire l'event listener principal ?
    - Assainir le CSS
    - Ajouter des animations

--- exercice1.js
+++ exercice1.js
@@ -1,12 +1,3 @@
-/**
- * Created by sstienface on 16/10/2018.
- */
-
-function log(what)
-{
-    console.log(what);
-}
-
 var liste = {
 
     articles : [
@@ -39,36 +30,28 @@ var liste = {
             return;
         }
 
-        var li = document.createElement('li'),
-            button_hide = document.createElement('button'),
-            button_delete = document.createElement('button');
-
-        li.innerHTML = article;
-        li.id = "li-" + id;
-
-        button_delete.innerHTML = "Supprimer";
-        button_delete.classList.add('delete-button');
-
-        button_hide.innerHTML = "Masquer";
-        button_hide.classList.add('hide-button');
-
-
-        li.appendChild(button_delete);
-        li.appendChild(button_hide);
+        var li = listItem.instantiate(article, id);
 
         this.renduHTML.appendChild(li);
-        liste.last_used_id++;
+        this.last_used_id++;
     },
 
     addUserArticle: function()
     {
         this.user_input = document.getElementById('user-input').value;
 
-        if (this.user_input.length > 0)
+        if (this.user_input.length > 0 && this.user_input.length < 20)
         {
             this.createListItem(this.user_input, this.last_used_id);
-            document.getElementById('user-input').value = '';
         }
+        else
+        // TODO Implementer throwAlert(message, color);
+        {
+            alert('input trop grand');
+        }
+
+        document.getElementById('user-input').value = '';
+
     },
 
     removeListItem: function(which)
@@ -86,10 +69,82 @@ var liste = {
         this.renduHTML.removeChild(which);
     },
 
-    hideListItem: function(which)
-    {
+    hideListItem: function(li)
         // TODO : A refaire entièrement (avec des animations) ?
+    {
+       listItem.hide(li);
+    },
 
+    moveItemDown: function(li)
+    {
+        var nextSibling = li.nextElementSibling;
+
+        if (nextSibling)
+        {
+            var detached = this.renduHTML.removeChild(nextSibling);
+            this.renduHTML.insertBefore(detached, li);
+        }
+    },
+
+    moveItemUp: function(li)
+    {
+        var previousSibling = li.previousSibling;
+
+        if (previousSibling)
+        {
+            var detached = this.renduHTML.removeChild(li);
+            this.renduHTML.insertBefore(detached, previousSibling);
+        }
+    }
+};
+
+
+var listItem = {
+
+    li : null,
+    button_hide : null,
+    button_delete : null,
+    arrow_up: null,
+    arrow_down: null,
+
+    init: function()
+    {
+        this.li = document.createElement('li');
+
+        this.arrow_up = document.createElement('i');
+        this.arrow_up.classList.add('fas');
+        this.arrow_up.classList.add('fa-caret-up');
+
+        this.arrow_down = document.createElement('i');
+        this.arrow_down.classList.add('fas');
+        this.arrow_down.classList.add('fa-caret-down');
+
+        this.button_delete = document.createElement('button');
+        this.button_delete.innerHTML = 'Supprimer';
+        this.button_delete.classList.add('delete-button');
+
+        this.button_hide = document.createElement('button');
+        this.button_hide.innerHTML = 'Masquer';
+        this.button_hide.classList.add('hide-button');
+    },
+
+    instantiate: function(article, id)
+    {
+        this.init();
+
+        this.li.innerHTML = article;
+        this.li.id = id;
+
+        this.li.appendChild(this.arrow_up);
+        this.li.appendChild(this.arrow_down);
+        this.li.appendChild(this.button_delete);
+        this.li.appendChild(this.button_hide);
+
+        return this.li;
+    },
+
+    hide: function(which)
+    {
         which.style.visibility = 'hidden';
 
         for (var button of which.children)
@@ -99,31 +154,41 @@ var liste = {
     }
 };
 
+
+
 liste.display();
 
 document.addEventListener('click', function(e)
 {
     var t = e.target;
 
-    if (t.tagName !== 'BUTTON') {
-        e.preventDefault();
-        return;
-    }
-
-    switch(t.id)
+    if (t.tagName == 'BUTTON')
     {
-        case "button-add":
-            liste.addUserArticle();
-            break;
-        case "button-delete-last":
-            liste.removeListItem("last");
-            break;
-        default:
-            if (t.classList.contains('hide-button')) {
-                liste.hideListItem(t.parentNode);
-            } else if (t.classList.contains('delete-button')) {
-                liste.removeListItem(t.parentNode);
-            }
+        switch(t.id)
+        {
+            case "button-add":
+                liste.addUserArticle();
+                break;
+            case "button-delete-last":
+                liste.removeListItem("last");
+                break;
+            default:
+                if (t.classList.contains('hide-button')) {
+                    liste.hideListItem(t.parentNode);
+                } else if (t.classList.contains('delete-button')) {
+                    liste.removeListItem(t.parentNode);
+                }
+        }
     }
-
+    else if (t.tagName == 'I')
+    {
+      if (t.classList.contains('fa-caret-down'))
+      {
+          liste.moveItemDown(t.parentNode);
+      }
+      else if (t.classList.contains('fa-caret-up'))
+      {
+          liste.moveItemUp(t.parentNode);
+      }
+    }
 });
